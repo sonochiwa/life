2b6e7ad1c2572d9a97c4939b51b1ff60687c2410
Рашид Мамаримов <92553@npo.local>
2023-10-19 16:43:26 +0600

    refactor: js and html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,44 +1,55 @@
-// Генерируем игровую сетку
-const gridY = document.querySelector('.grid-y');
-const gridX = document.querySelector('.grid-x');
-
-for (let i = 0; i < 24; i++) {
-    const lineY = document.createElement('div');
-    lineY.classList.add('line-y');
-    gridY.appendChild(lineY);
-
-    const lineX = document.createElement('div');
-    lineX.classList.add('line-x');
-    gridX.appendChild(lineX);
-}
-
-// Заполняем игровое пространство пустыми клетками
-const board = document.querySelector('.board');
-for (let i = 0; i < 576; i++) {
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    board.appendChild(cell);
-}
-
-// Создаем пустой массив 24x24
-const boardArray = new Array(24).fill(null).map(() => new Array(24).fill(null));
-
-// Заполняем массив случайными значениями true и null (в соотношении 1 к 10)
-for (let i = 0; i < 24; i++) {
-    for (let j = 0; j < 24; j++) {
-        const randomValue = Math.random() < 0.1 ? true : null;
-        boardArray[i][j] = randomValue;
+function generateGameGrid(gridDiv, lineDiv) {
+    const grid = document.querySelector(gridDiv)
+    for (let i = 0; i < 23; i++) {
+        const line = document.createElement("div")
+        line.classList.add(lineDiv)
+        grid.appendChild(line)
     }
 }
 
-// Находим все элементы .cell внутри .board
-const cells = document.querySelectorAll('.board .cell');
+function insertEmptyCells(cellsNumber) {
+    const board = document.querySelector('.board')
+    for (let i = 0; i < cellsNumber; i++) {
+        const cell = document.createElement('div')
+        cell.classList.add('cell')
+        board.appendChild(cell)
+    }
+}
 
-// Применяем класс "living" к соответствующим элементам .cell на основе массива
-for (let i = 0; i < 24; i++) {
-    for (let j = 0; j < 24; j++) {
-        if (boardArray[i][j] === true) {
-            cells[i * 24 + j].classList.add('living');
+function fillBoard(ratio, board) {
+    for (let i = 0; i < 24; i++) {
+        for (let j = 0; j < 24; j++) {
+            board[i][j] = Math.random() < ratio / 10
+            board[i][j] && setAsLiving(i, j)
         }
     }
 }
+
+function setAsLiving(i, j) {
+    cells[i * 24 + j].classList.add('living')
+}
+
+function setAsDead(i, j) {
+    cells[i * 24 + j].classList.add('dead')
+}
+
+function findCells() {
+    return document.querySelectorAll('.board .cell')
+}
+
+function makeEmptyBoard(size) {
+    return new Array(size).fill(null).map(() => new Array(size).fill(null))
+}
+
+// Генерируем игровую сетку
+generateGameGrid(".grid-x", "line-x")
+
+generateGameGrid(".grid-y", "line-y")
+
+insertEmptyCells(576) // Заполняем игровое пространство пустыми клетками
+
+const board = makeEmptyBoard(24) // Создаем пустой массив размера x * x и заполняем null
+
+const cells = findCells() // Находим все элементы .cell внутри .board
+
+fillBoard(1, board) // Случайным образом заполняем игровое пространство клетками
