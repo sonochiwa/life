94f5bccaa7080fe76261a6c720b12e973e9bdfc7
Rashid Mamarimov <sonochiwaxd@gmail.com>
2023-10-19 23:04:13 +0600

    feat: add design, bloor, and toggle living/empty cell after generate location

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,28 +9,38 @@ function generateGameGrid(gridDiv, lineDiv) {
 
 function insertEmptyCells(cellsNumber) {
     const board = document.querySelector('.board')
+    board.style["filter"] = "0px"
     for (let i = 0; i < cellsNumber; i++) {
         const cell = document.createElement('div')
         cell.classList.add('cell')
         board.appendChild(cell)
     }
+    return board
 }
 
-function fillBoard(ratio, board) {
+function setAsLiving(i, j, cells) {
+    return cells[i * 24 + j].classList.add('living')
+}
+
+function setAsDead(i, j, cells) {
+    return cells[i * 24 + j].classList.add('dead')
+}
+
+function fillBoard(board, cells) {
+    function getRatio() {
+        return document.getElementById('customRange').value
+    }
+
+    const ratio = getRatio()
+
     for (let i = 0; i < 24; i++) {
         for (let j = 0; j < 24; j++) {
+            cells[i * 24 + j].classList.remove('living')
+            cells[i * 24 + j].classList.remove('dead')
             board[i][j] = Math.random() < ratio / 10
-            board[i][j] && setAsLiving(i, j)
+            board[i][j] && setAsLiving(i, j, cells)
         }
     }
-}
-
-function setAsLiving(i, j) {
-    cells[i * 24 + j].classList.add('living')
-}
-
-function setAsDead(i, j) {
-    cells[i * 24 + j].classList.add('dead')
 }
 
 function findCells() {
@@ -41,15 +51,76 @@ function makeEmptyBoard(size) {
     return new Array(size).fill(null).map(() => new Array(size).fill(null))
 }
 
-// Генерируем игровую сетку
-generateGameGrid(".grid-x", "line-x")
+function handleCells(cells, board) {
+    // Добавляем обработчик клика для каждой клетки
+    cells.forEach((cell, index) => {
+        cell.addEventListener('click', () => {
+            if (!board[Math.floor(index / 24)][index % 24]) {
+                // Если клетка не активна (значение в массиве равно false), делаем её активной
+                cell.classList.add('living');
+                board[Math.floor(index / 24)][index % 24] = true;
+            } else {
+                cell.classList.remove('living');
+                board[Math.floor(index / 24)][index % 24] = null;
+            }
+        });
+    });
+}
 
-generateGameGrid(".grid-y", "line-y")
+let firstGeneration = true
 
-insertEmptyCells(576) // Заполняем игровое пространство пустыми клетками
+function generate() {
+    const start = document.getElementById('st')
+    start.classList.remove('disabled')
 
-const board = makeEmptyBoard(24) // Создаем пустой массив размера x * x и заполняем null
+    let cells
+    let boardArr
 
-const cells = findCells() // Находим все элементы .cell внутри .board
+    if (firstGeneration) {
+        let generation = document.getElementById('generation');
+        generation.textContent = String(Number(generation.textContent) + 1)
 
-fillBoard(1, board) // Случайным образом заполняем игровое пространство клетками
+        insertEmptyCells(576) // Заполняем игровое пространство пустыми клетками
+        boardArr = makeEmptyBoard(24) // Создаем пустой массив размера x * x и заполняем null
+
+        generateGameGrid(".grid-x", "line-x") // Генерируем игровую сетку
+        generateGameGrid(".grid-y", "line-y")
+
+        const board = document.querySelector('.board') // Снимаем блюр
+        board.classList.remove('blur')
+
+        cells = findCells() // Находим все элементы .cell внутри .board
+        fillBoard(boardArr, cells) // Случайным образом заполняем игровое пространство клетками
+
+        firstGeneration = false
+
+    } else {
+        boardArr = makeEmptyBoard(24) // Создаем пустой массив размера x * x и заполняем null
+        cells = findCells() // Находим все элементы .cell внутри .board
+        fillBoard(boardArr, cells) // Случайным образом заполняем игровое пространство клетками
+    }
+    handleCells(cells, boardArr)
+}
+
+function start() {
+    const generate = document.getElementById('gen')
+    generate.classList.add('disabled')
+
+    const start = document.getElementById('st')
+    start.classList.add('disabled')
+
+    const stop = document.getElementById('sp')
+    stop.classList.remove('disabled')
+
+}
+
+function stop() {
+    const generate = document.getElementById('gen')
+    generate.classList.remove('disabled')
+
+    const start = document.getElementById('st')
+    start.classList.remove('disabled')
+
+    const stop = document.getElementById('sp')
+    stop.classList.add('disabled')
+}
